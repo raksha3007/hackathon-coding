0e08f70f37e69910220a0e219b6856b7da35b877
Hariprasad <hariprasadgowda1998@gmail.com>
2026-02-20 06:19:15 -0500

    Updated backend tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,4 @@
 # Backend configuration for Terraform state
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "backend-tf"
-    storage_account_name = "backendtfstuse"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+  backend "azurerm" { }
 }
